caae868b067aaf9455806ae9796840daad6df4ea
ISHAN142005 <ishanbohra1414@gmail.com>
2026-03-31 19:00:56 +0530

    modification

--- MASTERCLASSES/DAY4/PointerApproachReOrder.cpp
+++ MASTERCLASSES/DAY4/PointerApproachReOrder.cpp
@@ -2,11 +2,13 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main()
+{
     string org = "xebia";
     int L = 0, R = org.size() - 1;
 
-    while (L < R) {
+    while (L < R)
+    {
         swap(org[L], org[R]);
         L++;
         R--;
